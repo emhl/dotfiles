61243eaab1c224da2c5d804dce1de7b2943bce05
Emhl <36165088+emhl@users.noreply.github.com>
2022-12-04 19:07:16 +0100

    automatically use colored versions in fish
    adds color to:
    diff, grep, ip, less and ls

---
+++ alias/color.fish
@@ -0,0 +1,6 @@
+#!/bin/fish
+alias -s diff='diff --color=auto'
+alias -s grep='grep --color=auto' 
+alias -s ip='ip -color=auto'
+set -xU LESS='-R --use-color -Dd+r$Du+b'
+alias -s ls='ls --color=auto'
