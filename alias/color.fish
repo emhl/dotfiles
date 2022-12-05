01a6e1bebe98874db76c04f933d90c0ef795ade5
Emhl <36165088+emhl@users.noreply.github.com>
2022-12-05 11:07:20 +0100

    update set syntax

--- alias/color.fish
+++ alias/color.fish
@@ -2,5 +2,5 @@
 alias -s diff='diff --color=auto'
 alias -s grep='grep --color=auto' 
 alias -s ip='ip -color=auto'
-set -xU LESS='-R --use-color -Dd+r$Du+b'
+set -xU LESS '-R --use-color -Dd+r$Du+b'
 alias -s ls='ls --color=auto'
