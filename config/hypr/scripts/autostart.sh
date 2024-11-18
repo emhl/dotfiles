7c79da0fe0a9dd3fac72513dce5cb3b2e4f2a842
emhl <emhl@steinr.eu>
2024-11-18 15:56:15 +0100

    add keyring to autostart

--- config/hypr/scripts/autostart.sh
+++ config/hypr/scripts/autostart.sh
@@ -2,3 +2,7 @@
 # kde-polkit-agent
 #/nix/store/$(ls -la /nix/store | grep polkit-kde-agent | grep '^d' | awk '{print $9}')/libexec/polkit-kde-authentication-agent-1 & 
 /usr/lib/polkit-kde-authentication-agent-1
+
+# keyring
+dbus-update-activation-environment --all
+gnome-keyring-daemon --start --components=secrets
